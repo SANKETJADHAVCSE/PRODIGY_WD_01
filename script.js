2a0268ce1ac4d301e1986bc50f30097d4671b4e9
sanket sadashiv jadhav <136482234+SANKETJADHAVCSE@users.noreply.github.com>
2024-07-19 13:45:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('nav ul li a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            navLinks.forEach(nav => nav.classList.remove('active'));
+            link.classList.add('active');
+        });
+    });
+});
